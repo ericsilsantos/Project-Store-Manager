ac50e16e17694f7bdc94bf92d73f72a97af475dc
Eric Santos <eric.silvasantos44@gmail.com>
2022-07-02 11:47:22 -0300

    Iniciando o projeto

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.listen('3000', () => {
+  console.log('Começando o projeto');
+})
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
